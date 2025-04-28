79d5e51294d87b08c6343fd8e05c1b617414ec28
b.waloe <dev@bettinamaria.at>
2025-04-28 13:05:36 +0200

    fixed return httpresponse not allowed

--- src/Control/MultisitesFrontController.php
+++ src/Control/MultisitesFrontController.php
@@ -16,86 +16,87 @@ use SilverStripe\CMS\Controllers\ModelAsController;
 class MultisitesFrontController extends ModelAsController {
 
 
-	/**
-	 * Overrides ModelAsController->getNestedController to find the nested controller
-	 * on a per-site basis
-	 **/
+    /**
+     * Overrides ModelAsController->getNestedController to find the nested controller
+     * on a per-site basis
+     **/
     public function getNestedController(): ContentController
     {
-		$request = $this->request;
-		$segment = $request->param('URLSegment');
-		$site    = Multisites::inst()->getCurrentSiteId();
+        $request = $this->request;
+        $segment = $request->param('URLSegment');
+        $site    = Multisites::inst()->getCurrentSiteId();
 
-		if(!$site) {
-			return $this->httpError(404);
-		}
+        if(!$site) {
+            return $this->httpError(404);
+        }
 
-		$page = SiteTree::get()->filter(array(
-			'ParentID'   => $site,
-			'URLSegment' => rawurlencode($segment)
-		));
-		$page = $page->first();
+        $page = SiteTree::get()->filter(array(
+            'ParentID'   => $site,
+            'URLSegment' => rawurlencode($segment)
+        ));
+        $page = $page->first();
 
-		if(!$page) {
-			// Check to see if linkmapping module is installed and if so, if there a map for this request.
-			if(class_exists('LinkMapping')){
-				$queryString = '?';
-				if ($request->requestVars()){
-					foreach($request->requestVars() as  $key => $value) {
-						if($key !='url'){$queryString .=$key.'='.$value.'&';}
-					}
-					$queryString =  rtrim($queryString,'&');
-				}
-				$link = ($queryString != '?' ? $request->getURL().$queryString : $request->getURL());
-				$link = trim(Director::makeRelative($link));
+        if(!$page) {
+            // Check to see if linkmapping module is installed and if so, if there a map for this request.
+            if(class_exists('LinkMapping')){
+                $queryString = '?';
+                if ($request->requestVars()){
+                    foreach($request->requestVars() as  $key => $value) {
+                        if($key !='url'){$queryString .=$key.'='.$value.'&';}
+                    }
+                    $queryString =  rtrim($queryString,'&');
+                }
+                $link = ($queryString != '?' ? $request->getURL().$queryString : $request->getURL());
+                $link = trim(Director::makeRelative($link));
 
-				$map  = LinkMapping::get()->filter('MappedLink', $link)->first();
+                $map  = LinkMapping::get()->filter('MappedLink', $link)->first();
 
-				if ($map) {
-					$this->response = new HTTPResponse();
-					$this->response->redirect($map->getLink(), 301);
-					return $this->response;
-				}
-			}
+                if ($map) {
+                    $this->response = new HTTPResponse();
+                    $this->response->redirect($map->getLink(), 301);
+                    return $this->response;
+                }
+            }
 
-			// use OldPageRedirector if it exists, to find old page
-			if(class_exists(OldPageRedirector::class)){
-				if($redirect = OldPageRedirector::find_old_page(array($segment), Multisites::inst()->getCurrentSite())){
-					$redirect = SiteTree::get_by_link($redirect);
-				}
-			}else{
-				$redirect = self::find_old_page($segment, $site);
-			}
+            // use OldPageRedirector if it exists, to find old page
+            if(class_exists(OldPageRedirector::class)){
+                if($redirect = OldPageRedirector::find_old_page(array($segment), Multisites::inst()->getCurrentSite())){
+                    $redirect = SiteTree::get_by_link($redirect);
+                }
+            }else{
+                $redirect = self::find_old_page($segment, $site);
+            }
 
-			if($redirect) {
-				$getVars = $request->getVars();
-				//remove the url var as it confuses the routing
-				unset($getVars['url']);
+            if($redirect) {
+                $getVars = $request->getVars();
+                //remove the url var as it confuses the routing
+                unset($getVars['url']);
 
-				$url = Controller::join_links(
-						$redirect->Link(
-							Controller::join_links(
-								$request->param('Action'),
-								$request->param('ID'),
-								$request->param('OtherID')
-							)
-						)
-					);
+                $url = Controller::join_links(
+                    $redirect->Link(
+                        Controller::join_links(
+                            $request->param('Action'),
+                            $request->param('ID'),
+                            $request->param('OtherID')
+                        )
+                    )
+                );
 
-				if(!empty($getVars)){
-					$url .= '?' . http_build_query($getVars);
-				}
+                if(!empty($getVars)){
+                    $url .= '?' . http_build_query($getVars);
+                }
 
-				$this->response->redirect($url, 301);
+                $this->response->redirect($url, 301);
 
-				return $this->response;
-			}
+                return ModelAsController::controller_for($redirect, $this->response);
 
-			return $this->httpError(404);
-		}
+            }
+
+            return $this->httpError(404);
+        }
 
 
-		return self::controller_for($page, $request->param('Action'));
-	}
+        return self::controller_for($page, $request->param('Action'));
+    }
 
 }
