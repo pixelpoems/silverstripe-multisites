f4174f7f5258b33cf484731288f69bf9487f003f
b.waloe <dev@bettinamaria.at>
2025-05-08 10:50:16 +0200

    Fixed insertAfter

--- src/Admin/MultisitesCMSPageAddController.php
+++ src/Admin/MultisitesCMSPageAddController.php
@@ -14,7 +14,7 @@ use SilverStripe\CMS\Controllers\CMSPageAddController;
  * An extension to the default page add interface which doesn't allow pages to
  * be created on the root.
  *
- * Can't do a direct extension because that doesn't allow us to control the form object itself. 
+ * Can't do a direct extension because that doesn't allow us to control the form object itself.
  *
  * @package silverstripe-multisites
  */
@@ -23,36 +23,38 @@ class MultisitesCMSPageAddController extends CMSPageAddController {
     private static $menu_title = 'Add page';
 
 
-	private static $allowed_actions = array(
-		'AddForm'
-	);
+    private static $allowed_actions = array(
+        'AddForm'
+    );
 
-	private static $url_priority = 43;
+    private static $url_priority = 43;
 
-	public function AddForm() {
-		$form   = parent::AddForm();
-		$fields = $form->Fields();
+    public function AddForm() {
+        $form   = parent::AddForm();
+        $fields = $form->Fields();
 
-		$fields->push(new HiddenField('Parent', null, true));
+        $fields->push(new HiddenField('Parent', null, true));
 
-		// Enforce a parent mode of "child" to correctly read the "allowed children".
+        // Enforce a parent mode of "child" to correctly read the "allowed children".
         $parentMode = $fields->dataFieldByName('ParentModeField');
 
-		$parentMode->removeByName('Top level');
+        $parentMode->removeByName('Top level');
         $parentMode->setValue('child');
-        
-		$fields->insertAfter($parent = new TreeDropdownField(
-			'ParentID', '', SiteTree::class, 'ID', 'TreeTitle'
-		), 'ParentModeField');
 
-		$parentID = $this->request->getVar('ParentID');
-		$parentID = $parentID ? $parentID : Multisites::inst()->getCurrentSiteId();
+        $treeDropdownField = new TreeDropdownField(
+            'ParentID', '', SiteTree::class, 'ID', 'TreeTitle'
+        );
 
-		$parent->setForm($form);
-		$parent->setValue((int)$parentID);
+        $fields->insertAfter('ParentModeField', $treeDropdownField);
 
-		$form->setValidator(new RequiredFields('ParentID'));
-		return $form;
-	}
+        $parentID = $this->request->getVar('ParentID');
+        $parentID = $parentID ? $parentID : Multisites::inst()->getCurrentSiteId();
+
+        $treeDropdownField->setForm($form);
+        $treeDropdownField->setValue((int)$parentID);
+
+        $form->setValidator(new RequiredFields('ParentID'));
+        return $form;
+    }
 
 }
